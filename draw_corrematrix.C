67dfcd2fff0a465618e9672e7e44720c22f14154
bean <bean@ip71-164.vpnip.cc.ntu.edu.tw>
2017-10-13 23:50:07 +0900

    correct the bug of ROOT by Chia-Te Chen

--- draw_corrematrix.C
+++ draw_corrematrix.C
@@ -17,18 +17,21 @@
 #include "TLegend.h"
 #include "TBranch.h"
 #include "TFile.h"
+#include "PlotStyle.h"
 #include <vector>
 
 void help();
 void draw_corrematrix(int argc, char* argv);
-Double_t getmean(TTree *tree, TString bname);
-Double_t getrms(TTree *tree, TString bname, Double_t bmean);
+Double_t branchvalue[1000];
+Double_t getmean(TTree *tree, Int_t bnum);
+Double_t getrms(TTree *tree, Int_t bnum, Double_t bmean);
+Double_t getXY(TTree *tree, Int_t bnumX, Int_t bnumY);
 
 
 using namespace std;
 void draw_corrematrix(int argc, char* argv[])
 {
-  if (argc<4){
+  if (argc<3){
     help();
     exit(1);
   }
@@ -82,53 +85,86 @@ void draw_corrematrix(int argc, char* argv[])
       (*corMat)(ibra,jbra) = 0.0;
     }
   }
+  
   vector<Double_t> meanX;
   vector<Double_t> rmsX;
+  vector<vector<Double_t>> XYX;
+  Double_t XY = 0.0;
   TString branchName;
+  TString branchName2;
   Double_t bmean,brms;
   double *Dval; //declare the Double_t pointer
   Dval=(double*)malloc(nbranch*sizeof(double*)); //allocate memory
   memset(Dval, 0.0, nbranch*sizeof(double*)); //initialize each elements to be zeros
   //fill diagonal elements of correlation matrix
+
+  for (Int_t i=0;i<nbranch;i++){
+    dataTree->SetBranchAddress(tbra->At(i)->GetName(), &(branchvalue[i]));
+  }
+  
   for (Int_t ibra=0;ibra<nbranch;ibra++){
-    branchName= tbra->At(ibra)->GetName();
-    bmean = getmean(dataTree,branchName);
-    brms = getrms(dataTree,branchName,bmean);
+    //branchName= tbra->At(ibra)->GetName();
+    //cout << ibra  << std::endl;
+    bmean = getmean(dataTree,ibra);
+    //cout << "???" << std::endl;
+    brms = getrms(dataTree,ibra,bmean);
+    //cout << ibra << " " << branchName << " mean is " << bmean <<std::endl;
+    //cout << ibra << " " << branchName << "  rms is " << brms <<std::endl;
+
     (*corMat)(ibra,ibra) = 100.0*brms*brms/(brms*brms); //1.0*100%
     meanX.push_back(bmean);
     rmsX.push_back(brms);
+    vector<Double_t> XYY;
+    for (Int_t jbra=0;jbra<ibra;jbra++){
+      //branchName2 = tbra->At(jbra)->GetName();
+      XY = getXY(dataTree, ibra, jbra);
+      XYY.push_back(XY);
+      //cout << branchName << ", " << branchName2 << ": "<< XY <<std::endl;
+      //cout << ibra << ", " << jbra << ": "<< XY <<std::endl;
+    }
+    XYX.push_back(XYY);
     //Set the branch to address
-    dataTree->SetBranchAddress(branchName, Dval+ibra);
+
+    branchName= tbra->At(ibra)->GetName();
+    cout << ibra << ": " << branchName << std::endl;
   }
   //std::cout<<"meanX :"<<meanX.at(1)<< std::endl; 
   Double_t Xi;
   Double_t Yj;
   //fill off-diagonal elements of correlation matrix
   //loop over all data tree entries
-  for (Int_t iev=0;iev<dataTree->GetEntries();iev++){
-    dataTree->GetEntry(iev);
-    
-    if (iev%5000==0){
-      std::cout<<"Read in "<<iev<<" events"<<std::endl;
-      //std::cout<<*(Dval+1)<<std::endl;
-    }							       
-    for (Int_t ibra=0;ibra<nbranch;ibra++){
-      for (Int_t jbra=0;jbra<ibra;jbra++){
-	Xi = *(Dval+ibra);
-	Yj = *(Dval+jbra);
-	(*corMat)(ibra,jbra) += 100.0*((Xi*Yj-meanX.at(ibra)*meanX.at(jbra))/dataTree->GetEntries())/(rmsX.at(ibra)*rmsX.at(jbra));
-	
-	(*corMat)(jbra,ibra) += 100.0*((Xi*Yj-meanX.at(ibra)*meanX.at(jbra))/dataTree->GetEntries())/(rmsX.at(ibra)*rmsX.at(jbra));
-      }
+
+  
+  for (Int_t ibra=0;ibra<nbranch;ibra++){
+    for (Int_t jbra=0;jbra<ibra;jbra++){
+      (*corMat)(ibra,jbra) = 100.0*(  ( (XYX.at(ibra)).at(jbra)-meanX.at(ibra)*meanX.at(jbra) )/(rmsX.at(ibra)*rmsX.at(jbra))  );
+      (*corMat)(jbra,ibra) = 100.0*(  ( (XYX.at(ibra)).at(jbra)-meanX.at(ibra)*meanX.at(jbra) )/(rmsX.at(ibra)*rmsX.at(jbra))  );
     }
   }
-  TFile *tfout = new TFile(argv[3],"RECREATE");
+
+  /*
+  cout<<"MeanX = "<<meanX.at(25)<<std::endl;
+  cout<<"MeanY = "<<meanX.at(21)<<std::endl;
+  cout<<" RMSX = "<<rmsX.at(25)<<std::endl;
+  cout<<" RMSY = "<<rmsX.at(21)<<std::endl;
+  cout<<" XY/N = "<< XY/dataTree->GetEntries()<<std::endl;
+  cout<<"  ANS = "<< (*corMat)(25,21)<<std::endl;
+  */
+
+  
+  char corre_Name_root[64];
+  sprintf(corre_Name_root, "%s_corre.root", argv[2]);
+
+  TFile *tfout = new TFile(corre_Name_root,"RECREATE");
   if(!tfout){
     std::cout <<"Error opening output file"<<argv[3]<<std::endl;
     return;
   }
+  
+
   //Save into root file
   corMat->Write();
+  
   //Draw correlation matrix plot
   TH2D *hcovmat = new TH2D("hcovmat","hcovmat",corMat->GetNcols(),corMat->GetColLwb(),1+corMat->GetColUpb(),corMat->GetNrows(),corMat->GetRowLwb(),1+corMat->GetRowUpb());
   //Set the bin contents
@@ -149,53 +185,71 @@ void draw_corrematrix(int argc, char* argv[])
   TCanvas *c1 = new TCanvas("c1","c1",600,600);
   hcovmat->LabelsOption("v"); //Xaxis(default) label vertical
   hcovmat->Draw("colz");
-  c1->Print("corre_plot.pdf",".pdf");
-  c1->Print("corre_plot.png",".png");
+
+  char corre_Name_pdf[64];
+  char corre_Name_png[64];
+  sprintf(corre_Name_pdf, "%s_correplot.pdf", argv[2]);
+  sprintf(corre_Name_png, "%s_corre.png", argv[2]);
+  c1->Print(corre_Name_pdf,".pdf");
+  c1->Print(corre_Name_png,".png");
   
   hcovmat->Write();
   tfout->Close();
   return;
 }
 
-Double_t getmean(TTree *tree, TString bname)
+Double_t getmean(TTree *tree, Int_t bnum)
 {
+  Double_t bsum=0.0;
   Double_t bmean=0.0;
-  /*
-  TString hist=">>histb";
-  tree->Draw(bname+hist,"");
-  TH1F *histbt = (TH1F*)gDirectory->Get("histb");
-  bmean = histbt->GetMean();
-  */
-  Double_t val;
-  tree->SetBranchAddress(bname, &val);
   for (Int_t iev;iev<tree->GetEntries();iev++){
     tree->GetEntry(iev);
-    bmean += val*1.0/tree->GetEntries();
+    
+    bmean += branchvalue[bnum]/tree->GetEntries();
+    //cout << " value is " << branchvalue[bnum] <<std::endl;
+    
   }
+  
+  //cout << " sum is " << bsum <<std::endl;
+  //bmean = bsum/tree->GetEntries();
   //del histbt;
   return bmean;
 }
 
-Double_t getrms(TTree *tree, TString bname, Double_t bmean)
+Double_t getrms(TTree *tree, Int_t bnum, Double_t bmean)
 {
   Double_t brms=0.0;
-  /*
-  TString hist=">>histb";
-  tree->Draw(bname+hist,"");
-  TH1F *histbt = (TH1F*)gDirectory->Get("histb");
-  brms = histbt->GetRMS();
-  */
-  Double_t val;
+  Double_t bsqr=0.0;
   Double_t bmeansqr=0.0;
-  tree->SetBranchAddress(bname, &val);
+
   for (Int_t iev;iev<tree->GetEntries();iev++){
     tree->GetEntry(iev);
-    bmeansqr+= val*val/tree->GetEntries();
+    bmeansqr += branchvalue[bnum]*branchvalue[bnum]/tree->GetEntries();
+    //cout << " value is " << branchvalue[bnum] <<std::endl;
   }
-
-  brms = TMath::Sqrt(bmeansqr-bmean);
+ 
+  //cout << " sqr is " << bsqr <<std::endl;
+  //bmeansqr = bsqr/tree->GetEntries();
+  //cout << " meansqr is " << bmeansqr <<std::endl;
+  brms = TMath::Sqrt(bmeansqr-bmean*bmean);//
   //del histbt;
   return brms;
+}
+
+Double_t getXY(TTree *tree, Int_t bnumX, Int_t bnumY)
+{
+  Double_t bbXY=0.0;
+  Double_t bbmeanXY=0.0;
+
+  for (Int_t iev;iev<tree->GetEntries();iev++){
+    tree->GetEntry(iev);
+    bbXY += branchvalue[bnumX]*branchvalue[bnumY];
+    //cout << " Xvalue is " << branchvalue[bnumX] <<std::endl;
+    //cout << " Yvalue is " << branchvalue[bnumY] <<std::endl;
+  }
+  bbmeanXY = bbXY/tree->GetEntries();
+
+  return bbmeanXY;
 }
 
 void help()
@@ -207,6 +261,7 @@ void help()
 
 int main(int argc, char* argv[])
 {
+  PlotStyle();
   draw_corrematrix(argc,argv);
   return 0;
 }
